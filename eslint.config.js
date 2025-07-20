dd60aad5a395e0ba308ae22f69bf3bc678dd9de7
Karey Higuera <karey.higuera@gmail.com>
2025-07-20 16:05:52 -0400

    add placeholder test command

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
 import js from '@eslint/js';
-import globals from 'globals';
+import { browser, es2022 } from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from '@typescript-eslint/eslint-plugin';
@@ -13,8 +13,8 @@ export default [
   {
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ecmaVersion: es2022,
+      globals: browser,
       parser: tsParser,
       parserOptions: {
         ecmaVersion: 'latest',

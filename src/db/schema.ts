0c5c814aba216de2b2b9848a72c042e2007f41ed
Karey Higuera <karey.higuera@gmail.com>
2025-07-20 22:03:17 -0400

    add drizzle and neon database (#7)

---
+++ src/db/schema.ts
@@ -0,0 +1,8 @@
+import { integer, pgTable, varchar } from 'drizzle-orm/pg-core';
+
+export const users = pgTable('users', {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar('name', { length: 255 }).notNull(),
+  email: varchar('email', { length: 255 }).notNull().unique(),
+  password: varchar('password', { length: 255 }).notNull(),
+});

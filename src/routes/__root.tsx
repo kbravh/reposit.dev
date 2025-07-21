4d262be0cbeaf9bc406e1312b4c136ea649d9dbd
Karey Higuera <karey.higuera@gmail.com>
2025-07-20 20:35:19 -0400

    add tailwind css

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -6,6 +6,7 @@ import {
   HeadContent,
   Scripts,
 } from '@tanstack/react-router';
+import appCss from '../styles/app.css?url'
 
 export const Route = createRootRoute({
   head: () => ({
@@ -21,6 +22,7 @@ export const Route = createRootRoute({
         title: 'Reposit',
       },
     ],
+    links: [{ rel: 'stylesheet', href: appCss }],
   }),
   component: RootComponent,
 });

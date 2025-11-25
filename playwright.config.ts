0972a38c879f8ea67006b576d8dc18dc5577d16f
Karey Higuera <karey.higuera@gmail.com>
2025-11-25 17:08:53 -0500

    remove ci specific config

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,12 +10,7 @@ config({ path: '.env.test' });
 export default defineConfig({
   testDir: './e2e',
   fullyParallel: true,
-
   forbidOnly: !!process.env.CI,
-
-  retries: process.env.CI ? 2 : 0,
-
-  workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
     baseURL: 'http://localhost:3000',
@@ -33,6 +28,5 @@ export default defineConfig({
     command: 'npm run dev',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
-    timeout: 90000,
   },
 });
